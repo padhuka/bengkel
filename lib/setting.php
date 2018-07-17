93c969dbdf691a15b687c6fb3b54095f93661443
dapras <dapras.solo@gmail.com>
2018-07-17 22:22:36 +0700

    'upload'

---
+++ lib/setting.php
@@ -0,0 +1,14 @@
+<?php 
+	// DATA BASE
+	$user="root"; // userid untuk akses database anda
+	$db="db_bpn_rajaampat"; // nama database anda.
+	$passsw=""; // password untuk akses database anda
+	// baris dibawah ini jangan diedit
+	$host="localhost:3356";
+
+	//WEB
+	$title="Aplikasi Arsip --Kantor Pertanahan Kabupaten Raja Ampat--";
+	$kabupaten="Kabupaten Raja Ampat";
+	$titlefooter="Badan Pertanahan Nasional Kabupaten Raja Ampat, Papua Barat";
+
+?>
