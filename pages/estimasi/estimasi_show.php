656c991e4223cac5742b96d3f712f473f0bba79c
dhuka <dhuka@ssonlab.com>
2018-08-01 16:26:18 +0700

    estmasi

--- pages/estimasi/estimasi_show.php
+++ pages/estimasi/estimasi_show.php
@@ -13,53 +13,74 @@
                     <div class="modal-header">
                         <h4 class="modal-title" id="myModalLabel" style="text-align: center;padding-right: 0px">Data Estimasi <button type="button" class="close" aria-label="Close" onclick="$('#ModalShow').modal('hide');"><span>&times;</span></button></h4>                    
                     </div>
-                  
-                    <div class="modal-body">
-                <table id="estimasishow" class="table table-condensed table-bordered table-striped table-hover">
-                <tbody>
-                <?php
+                  <?php
                                     $j=1;
-                                    $sqlcatat = "SELECT * FROM t_estimasi WHERE id_estimasi='$idestimasi'";
+                                    $sqlcatat = "SELECT * FROM t_estimasi e 
+                                                  left join t_customer c
+                                                  on e.fk_customer=c.id_customer
+                                                  where e.id_estimasi='$idestimasi'";
                                     $rescatat = mysql_query( $sqlcatat );
                                     while($catat = mysql_fetch_array( $rescatat )){
                                 ?>
-                       <td>
-                         <tr class="row">
-                        <td>
-                        <th class="col-sm-6">
-                        <tr> <th>No Estimasi</th> <td ><?php echo $catat['id_estimasi'];?></td></tr>
-                        <tr> <th>Tgl Masuk</th> <td ><?php echo $catat['tgl'];?></td></tr>
-                        <tr> <th>No Chasis</th>  <td ><?php echo $catat['fk_no_chasis'];?></td></tr>
-                        <tr> <th>No Mesin</th> <td ><?php echo $catat['fk_no_mesin'];?></td></tr>
-                        <tr> <th>No Polisi</th>   <td ><?php echo $catat['fk_no_polisi'];?></td> </tr>
-                        </th>
-                        </td>
-                        </tr>
-                       </td>
-                       <td>
-                         <tr class="row">
-                        <td>
-                        <th class="col-sm-2">
-                        <tr> <th>No Estimasi</th> <td ><?php echo $catat['id_estimasi'];?></td></tr>
-                        <tr> <th>Tgl Masuk</th> <td ><?php echo $catat['tgl'];?></td></tr>
-                        <tr> <th>No Chasis</th>  <td ><?php echo $catat['fk_no_chasis'];?></td></tr>
-                        <tr> <th>No Mesin</th> <td ><?php echo $catat['fk_no_mesin'];?></td></tr>
-                        <tr> <th>No Polisi</th>   <td ><?php echo $catat['fk_no_polisi'];?></td> </tr>
-                        </th>
-                        </td>
-                        </tr>
+                    <div class="modal-body">
+                      <div class="modal-title-detail">ESTIMASI PKB</div>
 
+                      <div class="row">
+                       <div class="col-sm-6">
+                       <table id="estimasishow" class="table table-condensed table-bordered table-striped table-hover">
+                       <td>
+                         <th class="col-sm-6">
+                        <tr> <th>No Estimasi</th> <td ><?php echo $catat['id_estimasi'];?></td></tr>
+                        <tr> <th>Tgl Masuk</th> <td ><?php echo $catat['tgl'];?></td></tr>
+                        <tr> <th>No Chasis</th>  <td ><?php echo $catat['fk_no_chasis'];?></td></tr>
+                        <tr> <th>No Mesin</th> <td ><?php echo $catat['fk_no_mesin'];?></td></tr>
+                        <tr> <th>No Polisi</th>   <td ><?php echo $catat['fk_no_polisi'];?></td> </tr>
+                        </th>
                        </td>
-                      
-                
-                       
-                        
-                        
-                    <?php }?>
+                      </table>
+                           </div>
+
+                            <div class="col-sm-6">
+                               <table id="estimasishow" class="table table-condensed table-bordered table-striped table-hover">
+                          <td>
+                         <th class="col-sm-6">
+                        <tr> <th>Kategori </th> <td ><?php echo $catat['kategori'];?></td></tr>
+                        <tr> <th>KM Masuk</th> <td ><?php echo $catat['km_masuk'];?></td></tr>
+                        <tr> <th>Asuransi</th>  <td ><?php echo $catat['fk_asuransi'];?></td></tr>
+                        <tr> <th>Nama Customer</th> <td ><?php echo $catat['nama'];?></td></tr>
+                        <tr> <th>Telp</th>   <td ><?php echo $catat['no_telp'];?></td> </tr>
+                        </th>
+                       </td>
+                               </table>
+
+
+                             <?php }?>
+                             </div>
+
+                      </div>
+                       <div class="modal-title-detail">NILAI ESTIMASI </div>
+
+                      <div class="row">
+                       <div class="col-sm-12">
+                       <table id="estimasishow" class="table table-condensed table-bordered table-striped table-hover">
+                       <td>
+                         <th class="col-sm-6">
+                        <tr> <th>Nilai Jasa</th><td >Jasa</td> <td >Jasa</td></tr>
+                        <tr> <th>Nilai Part</th><td >Part</td> <td >Part</td></tr>
+                        </th>
+                       </td>
+                      </table>
+                           </div>
+                      </div>
                    
-                </tfoot>
-              </table>
+               
+            
               
+
+
+
+
+
               <script>
             $('#estimasishow').DataTable();
           
@@ -82,9 +103,15 @@
     text-align: center;
     font-weight: bold;
   }
+  .modal-title-detail {
+    font-style: italic;
+    background-color: lightblue;
+    text-align: center;
+    font-weight: bold;
+  }
   .modal-dialog {
     margin-bottom: 0px;
     border: 3px;
-    width: 900px;
+    width: 800px;
   }
 </style>
