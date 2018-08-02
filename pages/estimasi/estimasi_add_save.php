5e2b8254409f6b6c45f894047afc6063f7cb57f4
dapras <dapras.solo@gmail.com>
2018-08-02 08:22:32 +0700

    idestimasi new format

--- pages/estimasi/estimasi_add_save.php
+++ pages/estimasi/estimasi_add_save.php
@@ -15,28 +15,33 @@
         $asuransi = trim($_POST['asuransi']);
         //message_back($id_estimasi);
         //$kodeawal = 'est_'.$hrini.'_';
-        $kodeawal = 'est_';
+        $hrn2= date('dmy' , strtotime($hrini));
+  //EST.BR.020818.000001
+        $kodeawal = 'EST.BR.'.$hrn2.'.';
         $sqljur = "SELECT * FROM t_estimasi WHERE id_estimasi LIKE '$kodeawal%' ORDER BY id_estimasi DESC";
         $resultjur = mysql_query( $sqljur );
         $jur = mysql_fetch_array( $resultjur );
         if (empty($jur['id_estimasi'])){
-            $kodeakhir = '001';
+            $kodeakhir = '000001';
         }else{
             # GENERATE
             $kode = $jur['id_estimasi'];
-            $pecah = explode('_',$kode);
-            $nilbaru = $pecah[1] + 1;
+            $pecah = explode('.',$kode);
+            $nilbaru = $pecah[3] + 1;
             $panj = strlen($nilbaru);
             //message_back($panj);
             switch($panj){
                 default : break;
-                case '1' : $kodeakhir='00'.$nilbaru; break;
-                case '2' : $kodeakhir='0'.$nilbaru; break;
-                case '3' : $kodeakhir=$nilbaru; break;
+                case '1' : $kodeakhir='00000'.$nilbaru; break;
+                case '2' : $kodeakhir='0000'.$nilbaru; break;
+                case '3' : $kodeakhir='000'.$nilbaru; break;
+                case '4' : $kodeakhir='00'.$nilbaru; break;
+                case '5' : $kodeakhir='0'.$nilbaru; break;
+                case '6' : $kodeakhir=$nilbaru; break;
             }
         }
         
-        $kodebaru = $kodeawal.$kodeakhir;         
+        $kodebaru = $kodeawal.$kodeakhir;     
 
         
             $sqltbemp = "INSERT INTO t_estimasi (id_estimasi,fk_no_chasis,fk_no_mesin,fk_no_polisi,km_masuk,fk_user,kategori,fk_customer,fk_asuransi) VALUES ('$kodebaru','$chasis','$mesin','$polisi','$kmmasuk','$uname','$kategori','$customer','$asuransi')";
