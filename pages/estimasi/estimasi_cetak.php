88e87512af018af229a76c6671baa575edcbcacc
dapras <dapras.solo@gmail.com>
2018-08-09 11:27:10 +0700

    estimasi cetak

--- pages/estimasi/estimasi_cetak.php
+++ pages/estimasi/estimasi_cetak.php
@@ -73,14 +73,14 @@
                                         <?php }?>
                                 </table>
                                  <table width="100%" align="center" border="0" cellspacing="0" cellpadding="0">
-                                            <tr><td colspan="2" align="center">Sub Total Jasa</td><td align="right"><?php echo rupiah2($catat['total_netto_harga_jasa']);?></td></tr>
-                                            <tr><td colspan="2" align="center">PPN</td><td align="right"><?php echo rupiah2((10/100)*$catat['total_netto_harga_jasa']);?></td></tr>
-                                            <tr><td colspan="2" align="center">Total Jasa</td><td align="right"><?php echo rupiah2((110/100)*$catat['total_netto_harga_jasa']);?></td></tr>
+                                            <tr><td colspan="2" align="right">Sub Total Jasa</td><td align="right" width="20%"><?php echo rupiah2($catat['total_netto_harga_jasa']);?></td></tr>
+                                            <tr><td colspan="2" align="right">PPN</td><td align="right"><?php echo rupiah2((10/100)*$catat['total_netto_harga_jasa']);?></td></tr>
+                                            <tr><td colspan="2" align="right">Total Jasa</td><td align="right"><?php echo rupiah2((110/100)*$catat['total_netto_harga_jasa']);?></td></tr>
                                 </table>
                                 <br>
 
                                  <table width="100%" align="center" border="0" cellspacing="0" cellpadding="0">
-                                   <tr><td align="center">GRAND TOTAL RP <?php echo rupiah2((110/100)*$catat['total_netto_harga_jasa']);?></td><td width="30%"></td></tr>
+                                   <tr><td align="right"><strong>GRAND TOTAL RP <?php echo rupiah2((110/100)*$catat['total_netto_harga_jasa']);?></strong></td><td width="10%"></td></tr>
                                  </table>
                                 <br><br>
                                 <table width="100%" align="center" border="0" cellspacing="0" cellpadding="0">
