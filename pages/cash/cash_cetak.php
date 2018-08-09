72d98e55d6232c9cfa1f6e6115be9322ef2fe692
dapras <dapras.solo@gmail.com>
2018-08-09 14:28:46 +0700

    cetak

--- pages/cash/cash_cetak.php
+++ pages/cash/cash_cetak.php
@@ -3,7 +3,7 @@
 <head>
   <title></title>
 </head>
-<body >
+<body onload="javascript:window.print()">
 <?php //onload="javascript:window.print()"
    // include_once '../../lib/sess.php';
     include_once '../../lib/config.php';
@@ -19,7 +19,7 @@
                                     $rescatat = mysql_query( $sqlcatat );
                                     $catat = mysql_fetch_array( $rescatat );
                                 ?>
-                                <table width="60%" align="center" border="0">
+                                <table width="100%" align="center" border="0">
                                   <tr>
                                     <td width="50%"><u style="font-size: 20px;"><strong>GEMILANG BODY & PAINT</strong><br>
                                     </u>
@@ -27,20 +27,20 @@
                                     Srondol Kulon Semarang
                                     </td>
                                     <td width="50%" align="center">
-                                    <h2>No. Kwitansi : <?php echo $nobukti;?></h2>
+                                    No. Kwitansi : <?php echo $nobukti;?>
                                     </td>
                                   </tr> 
                                 </table>
                       
-                                <hr width="60%" align="center" align="center">
+                                <hr width="100%" align="center" align="center">
                                 <br><br>
-                                <table width="60%" border="0"  align="center">
+                                <table width="100%" border="0"  align="center">
                                   <tr><td class="hurufe" width="30%">Terima Dari</td><td class="hurufe" width="2%">: </td><td class="huruf2"><?php echo $catat['diterima_dari'];?></td></tr>
                                   <tr><td class="hurufe">Uang Sejumlah</td><td class="hurufe">: </td><td class="huruf2">## <?php echo Terbilang($catat['total']);?> rupiah ##</td></tr>
                                   <tr><td  class="hurufe">Untuk Pembayaran</td><td class="hurufe">: </td><td class="huruf2"><?php echo $catat['keterangan'];?></td></tr>
                                 </table>
                                 <br>
-                                <table width="60%" border="0"  align="center">
+                                <table width="100%" border="0"  align="center">
                                     <tr>
                                       <td>
                                         <table width="50%" border="1"  align="left">
@@ -50,7 +50,7 @@
                                         </table>
                                       </td>
                                       <td>
-                                          <table width="60%" border="0"  align="center">
+                                          <table width="100%" border="0"  align="center">
                                             <tr>
                                               <td align="center">Semarang, <?php echo tampilTanggal(substr($catat['tgl_transaksi'],0,10));?><br><br><br><br>(&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)</td>
                                             </tr>
@@ -62,13 +62,13 @@
 
    <style type="text/css">
   .hurufe {
-    font-size: 26px;
+    font-size: 14px;
     font-weight: bold;
     font-family: monospace;
     text-align: right;
   }
   .huruf2 {
-    font-size: 26px;
+    font-size: 14px;
     font-weight: bold;
     font-family: monospace;
   }
