155eb20c8d28cbd8f8b3d58f10865128425c5615
dapras <dapras.solo@gmail.com>
2018-09-03 15:00:28 +0700

    kwitansi

--- pages/kwitansi/kwitansi_add_save.php
+++ pages/kwitansi/kwitansi_add_save.php
@@ -7,7 +7,8 @@
         $hrn2= date('dmy' , strtotime($hrini));
         $kodeawal2 = 'SI_BR.';
         $kodeawal = 'SI_BR.'.$hrn2.'.';
-        $sqljur = "SELECT * FROM t_kwitansi WHERE no_kwitansi LIKE '$kodeawal2%' ORDER BY no_kwitansi DESC";
+        //$sqljur = "SELECT * FROM t_kwitansi WHERE no_kwitansi LIKE '$kodeawal2%' ORDER BY no_kwitansi DESC";
+        $sqljur = "SELECT * FROM t_kwitansi ORDER BY tgl_kwitansi DESC";
         $resultjur = mysql_query( $sqljur );
         $jur = mysql_fetch_array( $resultjur );
         if (empty($jur['no_kwitansi'])){
