c6ca2ad3107de48249084bba4b0f584be956abb2
dhuka <dhuka@ssonlab.com>
2018-10-02 13:23:47 +0700

    update load estimasi kwitansi or

--- pages/kwitansior/kwitansior_estimasi_tab.php
+++ pages/kwitansior/kwitansior_estimasi_tab.php
@@ -28,8 +28,9 @@
                              
                                    $sqlcatat = "SELECT * FROM t_estimasi e 
                                    LEFT JOIN t_customer c ON e.fk_customer=c.id_customer
-                                   LEFT JOIN t_kwitansi_or k ON k.fk_estimasi=e.id_estimasi
-                                   WHERE e.tgl_batal='0000-00-00 00:00:00' AND e.approved='1' AND kategori='Asuransi' AND k.fk_estimasi IS NULL ORDER BY e.id_estimasi DESC";
+                                   LEFT JOIN ( select * from t_kwitansi_or WHERE tgl_batal='0000-00-00 00:00:00') as k  ON k.fk_estimasi=e.id_estimasi
+                                   WHERE e.tgl_batal='0000-00-00 00:00:00' AND e.approved='1' AND e.kategori='Asuransi' 
+                                   AND k.fk_estimasi IS NULL ORDER BY e.id_estimasi DESC";
                                    $rescatat = mysql_query( $sqlcatat );
                                     while($catat = mysql_fetch_array( $rescatat )){
                                 ?>
