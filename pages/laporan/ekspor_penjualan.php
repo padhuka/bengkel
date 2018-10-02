9f83a3196f4bbded868d2dfbe53cba657441cc31
dhuka <dhuka@ssonlab.com>
2018-10-02 21:04:13 +0700

    fix laporan

--- pages/laporan/ekspor_penjualan.php
+++ pages/laporan/ekspor_penjualan.php
@@ -22,7 +22,7 @@ header("Content-Disposition: attachment; filename=reportpenjualan.xls");
                                     </td>                                   
                                   </tr>                                   
                                 </table>
-                                    <span style="font-size: 20px;font-weight: bold;"><center>Laporan PKB</center></span>
+                                    <span style="font-size: 20px;font-weight: bold;"><center>Laporan Penjualan </center></span>
                                      <span style="font-size: 20px;font-weight: bold;"><center> Per Tgl <?php echo date('d-m-Y' , strtotime($_GET['tgl1']));echo ' s/d '; echo date('d-m-Y' , strtotime($_GET['tgl2'])); ?></center></span>
                                 <br>
       <table id="tablepkb1" class="table table-condensed table-bordered table-striped table-hover">
