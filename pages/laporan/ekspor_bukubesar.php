25c72ce529887764404ac2a130e0943ef312ea71
dapras <dapras.solo@gmail.com>
2019-02-19 08:49:00 +0700

    filter status batal

--- pages/laporan/ekspor_bukubesar.php
+++ pages/laporan/ekspor_bukubesar.php
@@ -1,9 +1,9 @@
 <?php
 // Fungsi header dengan mengirimkan raw data excel
-///header("Content-type: application/vnd-ms-excel");
+header("Content-type: application/vnd-ms-excel");
  
 // Mendefinisikan nama file ekspor "hasil-export.xls"
-///header("Content-Disposition: attachment; filename=reportbukubesar.xls");
+header("Content-Disposition: attachment; filename=reportbukubesar.xls");
  
 // Tambahkan table
 //include 'data.php';
