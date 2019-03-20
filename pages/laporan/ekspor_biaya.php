687e4904508ed6d135e2558effe5ed43baffad06
dapras <dapras.solo@gmail.com>
2019-03-20 09:38:27 +0700

    lap baiaya

--- pages/laporan/ekspor_biaya.php
+++ pages/laporan/ekspor_biaya.php
@@ -1,13 +1,21 @@
 
 <?php
 // Fungsi header dengan mengirimkan raw data excel
-///header("Content-type: application/vnd-ms-excel");
+header("Content-type: application/vnd-ms-excel");
  
 // Mendefinisikan nama file ekspor "hasil-export.xls"
-///header("Content-Disposition: attachment; filename=reportbukubesar.xls");
+header("Content-Disposition: attachment; filename=reportbukubesar.xls");
  
 // Tambahkan table
 //include 'data.php';php
+/*
+SELECT * FROM t_akun A 
+LEFT JOIN (SELECT * FROM t_acc_cash WHERE no_bukti<>'' AND substring(tr_date,1,4)='2019') AS B ON A.coa=B.ref_akun
+LEFT JOIN (SELECT * FROM t_acc_bank WHERE no_bukti<>'' AND substring(tr_date,1,4)='2019') AS C ON A.coa=C.ref_akun
+WHERE A.coa LIKE '51%'
+GROUP BY A.coa
+ORDER BY A.coa ASC
+*/
 
 ?>
 								      <?php
@@ -28,7 +36,7 @@
                                     </td>                                   
                                   </tr>                                   
                                 </table>
-                                    <span style="font-size: 20px;font-weight: bold;"><center>Laporan Buku Besar </center></span>
+                                    <span style="font-size: 20px;font-weight: bold;"><center>Laporan Biaya </center></span>
                                      <span style="font-size: 20px;font-weight: bold;"><center> Tahun <?php echo date('Y' , strtotime($_GET['tgl1']));?></center></span>
                                 <br>
       <table id="tablepkb1" class="table table-condensed table-bordered table-striped table-hover">
@@ -60,32 +68,100 @@
                                    
                                 ?>
                                 <?php 
-                                  $sqlcatat = "SELECT A.coa, A.description AS nm FROM t_akun A
-                                  LEFT JOIN t_acc_cash B ON A.coa=B.fk_akun
-                                  LEFT JOIN t_acc_cash C ON A.coa=C.fk_akun
-                                  WHERE B.no_bukti<>'' AND C.no_bukti<>'' AND substring(B.tr_date,1,4)='$tgl1' AND substring(C.tr_date,1,4)='$tgl1' AND substring(A.coa,1,2)=51
-                                  GROUP BY A.coa";
-                                  //AND A.transaction_type='D' AND A.description LIKE '%kas%'
-                                  $rescatat = mysql_query( $sqlcatat );
+                                  $sqlcatat = "SELECT * FROM t_akun WHERE coa LIKE '51%'";
+                                  $rescatat = mysql_query($sqlcatat);
                                   while($catat = mysql_fetch_array( $rescatat )){
+                                    //cek
+                                    $t_acc_cash= mysql_fetch_array(mysql_query("SELECT ref_akun,sum(amount) AS jmlcash1 FROM t_acc_cash WHERE substring(tr_date,1,4)='$tgl1' AND ref_akun='$catat[coa]' AND status<>'Batal' AND no_bukti<>''") );
+                                    $t_acc_bank= mysql_fetch_array(mysql_query("SELECT ref_akun,sum(amount) AS jmlbank1 FROM t_acc_bank WHERE substring(tr_date,1,4)='$tgl1' AND ref_akun='$catat[coa]' AND status<>'Batal' AND no_bukti<>''") );
+                                    if ($t_acc_cash['ref_akun'] || $t_acc_bank['ref_akun']){
                                 ?>
                         <tr>
                             <td><?php echo $j++;?></td>
-                            <td><?php echo $catat['nm'];?></td>
+                            <td><?php echo $catat['description'];?></td>
                             <?php
                             for ( $i = 1; $i <= 12; $i ++ ) {
                               $blne=date('m',strtotime("$i/12/10"));
                               $peri=$tgl1.'-'.$blne;
-                              $sqlsum="SELECT sum(B.amount) AS jmle FROM t_akun A
-                              LEFT JOIN t_acc_cash B ON A.coa=B.fk_akun
-                              LEFT JOIN t_acc_cash C ON A.coa=C.fk_akun
-                              WHERE B.no_bukti<>'' AND C.no_bukti<>'' AND substring(B.tr_date,1,7)='$peri' AND substring(C.tr_date,1,7)='$peri' AND A.coa='$catat[coa]'";
+                              //cash
+                              $sqlsum="SELECT sum(amount) AS jmlcashbln FROM t_acc_cash WHERE no_bukti<>''  AND status<>'Batal' AND substring(tr_date,1,7)='$peri' AND ref_akun='$catat[coa]'";
                               $hsum=mysql_fetch_array(mysql_query($sqlsum));
+
+                              //bank
+                              $sqlsum2="SELECT sum(amount) AS jmlbankbln FROM t_acc_bank WHERE no_bukti<>''  AND status<>'Batal' AND substring(tr_date,1,7)='$peri' AND ref_akun='$catat[coa]'";
+                              $hsum2=mysql_fetch_array(mysql_query($sqlsum2));
                               //echo "<td>".date( 'm', strtotime( "$i/12/10" ) )."</td>";
-                              echo "<td>".rupiah2($hsum['jmle'])."</td>";
+                              echo "<td align=right>".rupiah2($hsum['jmlcashbln']+$hsum2['jmlbankbln'])."</td>";
                             } ?>
                         </tr>
-                    <?php }?>
+                    <?php 
+                                    }
+                      }?>
+
+                      <?php 
+                                  $sqlcatat = "SELECT * FROM t_akun WHERE coa LIKE '61%'";
+                                  $rescatat = mysql_query($sqlcatat);
+                                  while($catat = mysql_fetch_array( $rescatat )){
+                                    //cek
+                                    $t_acc_cash= mysql_fetch_array(mysql_query("SELECT ref_akun,sum(amount) AS jmlcash2 FROM t_acc_cash WHERE substring(tr_date,1,4)='$tgl1' AND ref_akun='$catat[coa]' AND status<>'Batal' AND no_bukti<>''") );
+                                    $t_acc_bank= mysql_fetch_array(mysql_query("SELECT ref_akun,sum(amount) AS jmlbank2 FROM t_acc_bank WHERE substring(tr_date,1,4)='$tgl1' AND ref_akun='$catat[coa]' AND status<>'Batal' AND no_bukti<>''") );
+                                    if ($t_acc_cash['ref_akun'] || $t_acc_bank['ref_akun']){
+                                ?>
+                        <tr>
+                            <td><?php echo $j++;?></td>
+                            <td><?php echo $catat['description'];?></td>
+                            <?php
+                            for ( $i = 1; $i <= 12; $i ++ ) {
+                              $blne=date('m',strtotime("$i/12/10"));
+                              $peri=$tgl1.'-'.$blne;
+                              //cash
+                              $sqlsum="SELECT sum(amount) AS jmlcashbln FROM t_acc_cash WHERE no_bukti<>''  AND status<>'Batal' AND substring(tr_date,1,7)='$peri' AND ref_akun='$catat[coa]'";
+                              $hsum=mysql_fetch_array(mysql_query($sqlsum));
+
+                              //bank
+                              $sqlsum2="SELECT sum(amount) AS jmlbankbln FROM t_acc_bank WHERE no_bukti<>''  AND status<>'Batal' AND substring(tr_date,1,7)='$peri' AND ref_akun='$catat[coa]'";
+                              $hsum2=mysql_fetch_array(mysql_query($sqlsum2));
+                              //echo "<td>".date( 'm', strtotime( "$i/12/10" ) )."</td>";
+                              echo "<td align=right>".rupiah2($hsum['jmlcashbln']+$hsum2['jmlbankbln'])."</td>";
+                            } ?>
+                        </tr>
+                    <?php 
+                                    }
+                      }?>
                     
+                     
+
+                      <?php 
+                                  $sqlcatat = "SELECT * FROM t_akun WHERE coa LIKE '81%'";
+                                  $rescatat = mysql_query($sqlcatat);
+                                  while($catat = mysql_fetch_array( $rescatat )){
+                                    //cek
+                                    $t_acc_cash= mysql_fetch_array(mysql_query("SELECT ref_akun,sum(amount) AS jmlcash3 FROM t_acc_cash WHERE substring(tr_date,1,4)='$tgl1' AND ref_akun='$catat[coa]' AND status<>'Batal' AND no_bukti<>''") );
+                                    $t_acc_bank= mysql_fetch_array(mysql_query("SELECT ref_akun,sum(amount) AS jmlbank3 FROM t_acc_bank WHERE substring(tr_date,1,4)='$tgl1' AND ref_akun='$catat[coa]' AND status<>'Batal' AND no_bukti<>''") );
+                                    if ($t_acc_cash['ref_akun'] || $t_acc_bank['ref_akun']){
+                                ?>
+                        <tr>
+                            <td><?php echo $j++;?></td>
+                            <td><?php echo $catat['description'];?></td>
+                            <?php
+                            for ( $i = 1; $i <= 12; $i ++ ) {
+                              $blne=date('m',strtotime("$i/12/10"));
+                              $peri=$tgl1.'-'.$blne;
+                              //cash
+                              $sqlsum="SELECT sum(amount) AS jmlcashbln FROM t_acc_cash WHERE no_bukti<>''  AND status<>'Batal' AND substring(tr_date,1,7)='$peri' AND ref_akun='$catat[coa]'";
+                              $hsum=mysql_fetch_array(mysql_query($sqlsum));
+
+                              //bank
+                              $sqlsum2="SELECT sum(amount) AS jmlbankbln FROM t_acc_bank WHERE no_bukti<>''  AND status<>'Batal' AND substring(tr_date,1,7)='$peri' AND ref_akun='$catat[coa]'";
+                              $hsum2=mysql_fetch_array(mysql_query($sqlsum2));
+                              //echo "<td>".date( 'm', strtotime( "$i/12/10" ) )."</td>";
+                              echo "<td align=right>".rupiah2($hsum['jmlcashbln']+$hsum2['jmlbankbln'])."</td>";
+                            } ?>
+                        </tr>
+                    <?php 
+                                    }
+                      }?>
+
+
                 </tfoot>
               </table>
