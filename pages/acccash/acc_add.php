1e07afa11904083ae3ac52a27a98b4701a601880
dhuka <dhuka@ssonlab.com>
2019-05-13 10:01:11 +0700

    pages/acccash/acc_add.php

--- pages/acccash/acc_add.php
+++ pages/acccash/acc_add.php
@@ -29,7 +29,7 @@
                 <tbody>
                 <?php
                                     $j=1;
-                                    $sqlcatat = "SELECT * FROM t_akun WHERE coa LIKE '61.%' ORDER BY id ASC";
+                                    $sqlcatat = "SELECT * FROM t_akun WHERE (coa LIKE '61.%' or coa like '51.%') ORDER BY id ASC";
                                     $rescatat = mysql_query( $sqlcatat );
                                     while($catat = mysql_fetch_array( $rescatat )){
                                 ?>
